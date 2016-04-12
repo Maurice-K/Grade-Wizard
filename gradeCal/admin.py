279ec05ab7cda18a45fe40684da5ecc62ead62ba
Maurice-K <maurice.i.kelly@gmail.com>
2016-04-11 23:40:49 -0500

    DB Schema change and admin site accesibility added

--- gradeCal/admin.py
+++ gradeCal/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Student, Course, CourseAssessment, GradeAssessment
+
+admin.site.register(Student)
+admin.site.register(Course)
+admin.site.register(CourseAssessment)
+admin.site.register(GradeAssessment)
--- gradeCal/models.py
+++ gradeCal/models.py
@@ -13,13 +13,17 @@ class Student(models.Model):
 	def __str__(self):
 		return self.first_name + " " + self.last_name
 
-#Ask abhaya how to get one field to have multiple choicesss
+
 class Course(models.Model):
 	student = models.ForeignKey(Student)
 	title = models.CharField(max_length=50)
 
+
 	def __str__(self):
 		return self.title
+
+#Table that handles each course's grade distribution.
+#Ex: Final worth 35 percent, Midterm worth 30 percent, Homework/Assignments worth 15 percent, Quizzes worth 20 percent
 
 class CourseAssessment(models.Model):
 	course = models.ForeignKey(Course)
@@ -27,13 +31,19 @@ class CourseAssessment(models.Model):
 	percentage = models.IntegerField(default=0)
 
 	def __str__(self):
-		return self.title
+		self.title
 
+
+#Grades that belong in course assessment
+#Ex: Homework #1 scored 30/40, Midterm scored 85/100
 class GradeAssessment(models.Model):
 	course_assessment = models.ForeignKey(CourseAssessment)
 	title = models.CharField(max_length=50)
 	score = models.IntegerField(default=0)
 	max_score = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.title
 	
 
 
